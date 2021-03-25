309781991f9d79cea0f4f0bb68e4b53338e799dd
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-25 20:45:55 -0300

    Entrega de la actividad 3, a pedido del docente.

---
+++ Actividad_03/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Actividad_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lea las teclas presionadas y finalice al presionar la tecla “X”.
+            string teclas;
+            string finalizar;
+            do
+            {
+                Console.WriteLine("Presione las teclas que requiere: ");
+                teclas = Console.ReadLine();
+                Console.WriteLine("\r");
+
+                Console.WriteLine("Las teclas que escribio son: " + teclas);
+                Console.ReadLine();
+                Console.WriteLine("\r");
+
+                Console.WriteLine("¿Desea finalizar el programa? (X = No)");
+                finalizar = Console.ReadLine();
+
+            } while (finalizar == " x ");
+        }
+
+    }
+}
