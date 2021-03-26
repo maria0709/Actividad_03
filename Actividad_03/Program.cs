c410a333d7be246e0815bad9f6baef5395fdeaf0
MARIA RENEÉ ROJAS ARCAYNE <88RO94097790@campus.economicas.uba.ar>
2021-03-26 01:19:39 -0300

    Entrega de la actividad 3, a pedido del docente.

--- Actividad_03/Program.cs
+++ Actividad_03/Program.cs
@@ -11,26 +11,25 @@ namespace Actividad_03
         static void Main(string[] args)
         {
             // Lea las teclas presionadas y finalice al presionar la tecla “X”.
-            string teclas;
             
+            string teclas;
+ 
             do
             {
-
                Console.WriteLine("Presione las teclas que requiere: ");
-                teclas= Console.ReadLine();
-                Console.WriteLine("\r");
+               teclas= Console.ReadLine();
+               Console.WriteLine("\r");
 
-                Console.WriteLine("Las teclas que escribio son: " + teclas);
-                Console.WriteLine("\r");
+               Console.WriteLine("Las teclas que escribio son: " + teclas);
+               Console.WriteLine("\r");
                
-                Console.WriteLine("Para finalizar el programa, debe ingresar la letra 'x'");
-                Console.WriteLine("\r");
+               Console.WriteLine("Para finalizar el programa, debe ingresar la letra 'x'");
+               Console.WriteLine("\r");
 
-               teclas = Console.ReadLine();
-                
-            } while (teclas.ToLower() != "x");
+               teclas = Console.ReadLine();   
+            } 
+            while (teclas.ToUpper () != "X");
         }
-
 
     }
 }
